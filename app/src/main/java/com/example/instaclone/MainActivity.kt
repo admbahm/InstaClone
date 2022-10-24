4e939061e9d1bf4b7ff8c59c77aeab1f5b68ab80
admbahm <admbaum@gmail.com>
2022-10-24 12:32:13 -0500

    just getting things set up

--- app/src/main/java/com/example/instaclone/MainActivity.kt
+++ app/src/main/java/com/example/instaclone/MainActivity.kt
@@ -8,5 +8,6 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         println("theres something a miss")
+        println("theres something a miss")
     }
 }
